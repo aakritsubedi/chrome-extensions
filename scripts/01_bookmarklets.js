c6d4bed89856dc1f8aace943d3cfc993973da533
Aakrit Subedi <aakritsubedi9@gmail.com>
2019-11-03 20:48:44 +0545

    Bookmarklets

---
+++ scripts/01_bookmarklets.js
@@ -0,0 +1,6 @@
+(function (){
+    let imgs = document.getElementsByTagName('img');
+    for(let i=0;i<imgs.length;i++){
+        imgs[i].setAttribute('src','http://aakritsubedi.com.np/images/profile_new.jpg');
+    }
+})();
